a67a503e9bd7c518426e64d57b0cbc7d91057db2
Robert Bücker <robert.buecker@mpsd.mpg.de>
2021-01-28 22:54:29 +0100

    exiting sequencer stops putzini

--- sequencer.py
+++ sequencer.py
@@ -42,50 +42,59 @@ steps = steps[int(argv[1]):]
 
 arka_state = {}
 putzini_state = {}
+try:
 
-for ii, s in enumerate(steps):
-    print(f'------- STEP {ii}:', s['name'], '-------')
-    if s['comment'] is not None:
-        print(f'[{s["comment"]}]')
-    
-    # print(s)
-    
-    if s['parameter']['beamer'] is not None:
-        for k in ['Regie', 'Arka', 'Putzini']:
-            if k in s['parameter']['beamer']:
-                print(f'{k} says ---')
-                print(s['parameter']['beamer'][k])
-                print('---')
-        if 'Clear' in s['parameter']['beamer']:
-            print('--- Beamer cleared')
+    for ii, s in enumerate(steps):
+        
             
-    if s['parameter']['putzini'] is not None:
-        client.publish('putzini/commands', json.dumps(s['parameter']['putzini']))
-        ptrig = False
-        putzini_state.update({k: v for k, v in s['parameter']['putzini'].items() if v is not None})
-    
-    if s['parameter']['arka'] is not None:
-        arka_state.update({k: v for k, v in s['parameter']['arka'].items() if v is not None})
-    
-    print(f'Arka is doing: {arka_state}')
-    print(f'Putzini is doing: {putzini_state}')
-    
-    if s['terminationCondition'] == 'Duration':
-        t = float(s['duration'])
-        t = t if s['deltaDuration'] is None else t + random.uniform(-s['deltaDuration'], s['deltaDuration'])
-        print(f'Running step for {t:.1f} s...')
-        sleep(t)
-    elif s['terminationCondition'] == 'WaitForArka':
-        input('Waiting for Arka. Press Enter to continue...')
-    elif s['terminationCondition'] == 'WaitForMusic':
-        print('Waiting for music...')
-        while not mtrig:
-            sleep(0.1)
-        mtrig = False
-    elif s['terminationCondition'] == 'WaitForPutzini':
-        print('Waiting for Putzini...')
-        while not ptrig:
-            sleep(0.1)
-        ptrig = False
-    else:
-        raise ValueError(f'Unknown termination condition: {s["terminationCondition"]}.')
+            print(f'------- STEP {ii}:', s['name'], '-------')
+            if s['comment'] is not None:
+                print(f'[{s["comment"]}]')
+            
+            # print(s)
+            
+            if s['parameter']['beamer'] is not None:
+                for k in ['Regie', 'Arka', 'Putzini']:
+                    if k in s['parameter']['beamer']:
+                        print(f'{k} says ---')
+                        print(s['parameter']['beamer'][k])
+                        print('---')
+                if 'Clear' in s['parameter']['beamer']:
+                    print('--- Beamer cleared')
+                    
+            if s['parameter']['putzini'] is not None:
+                client.publish('putzini/commands', json.dumps(s['parameter']['putzini']))
+                ptrig = False
+                putzini_state.update({k: v for k, v in s['parameter']['putzini'].items() if v is not None})
+            
+            if s['parameter']['arka'] is not None:
+                arka_state.update({k: v for k, v in s['parameter']['arka'].items() if v is not None})
+            
+            print(f'Arka is doing: {arka_state}')
+            print(f'Putzini is doing: {putzini_state}')
+            
+            if s['terminationCondition'] == 'Duration':
+                t = float(s['duration'])
+                t = t if s['deltaDuration'] is None else t + random.uniform(-s['deltaDuration'], s['deltaDuration'])
+                print(f'Running step for {t:.1f} s...')
+                sleep(t)
+            elif s['terminationCondition'] == 'WaitForArka':
+                input('Waiting for Arka. Press Enter to continue...')
+            elif s['terminationCondition'] == 'WaitForMusic':
+                print('Waiting for music...')
+                while not mtrig:
+                    sleep(0.1)
+                mtrig = False
+            elif s['terminationCondition'] == 'WaitForPutzini':
+                print('Waiting for Putzini...')
+                while not ptrig:
+                    sleep(0.1)
+                ptrig = False
+            else:
+                raise ValueError(f'Unknown termination condition: {s["terminationCondition"]}.')
+        
+except KeyboardInterrupt:
+    print('Sending STOP to Putzini...')
+    client.publish('putzini/commands', json.dumps({'move': 'stop()'}))
+    sleep(1)
+    print('Baba.')
