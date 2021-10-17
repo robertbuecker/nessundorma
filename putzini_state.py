a28982b711b8e64d93917d8f5fb791f3f41768a5
Putzini Jetson <robert.buecker@mpsd.mpg.de>
2021-10-17 17:00:54 +0200

    change state to Error

--- putzini_state.py
+++ putzini_state.py
@@ -47,8 +47,8 @@ class PutziniState:
         self.publish()
 
     def set_error(self, message=''):
-        # self.action = 'Error'
-        self.action = 'ErrorDontStop'
+        self.action = 'Error'
+        # self.action = 'ErrorDontStop'
         self.message = message
         # TODO to state the obvious...
         self.logger.critical('Putzini set to error state!')
